6f2d93c31aa801685586c819833a69e729328c91
Brent Jackson <jxnblk@gmail.com>
2019-03-23 17:14:02 -0400

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Styled System Extras
+
+Legacy packages and experiments
+
+
+- [@styled-system/typography](/packages/typography)
+- [clean-element](/packages/clean-element)
+- [clean-tag](/packages/clean-tag)
+- [system-classnames](/packages/system-classnames)
+- [system-docs](/packages/system-docs)
+- [system-loader](/packages/system-loader)
---
+++ packages/typography/src/reset.js
@@ -0,0 +1,24 @@
+// base style reset - useful for markdown content
+export const reset = {
+  img: {
+    maxWidth: '100%',
+    height: 'auto',
+  },
+  'h1 a, h2 a, h3 a, h4 a, h5 a, h6 a': {
+    color: 'inherit',
+  },
+  'blockquote p': {
+    fontSize: 'inherit',
+  },
+  'pre code': {
+    fontSize: 'inherit',
+    color: 'inherit',
+    backgroundColor: 'inherit',
+  },
+  'li > p': {
+    fontSize: 'inherit',
+    margin: 0,
+  },
+}
+
+export default reset
