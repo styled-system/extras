f6e1d95248973c912985f35912e20fe49812b6db
Brent Jackson <jxnblk@gmail.com>
2019-03-26 12:58:37 -0400

    Adjust output styles

--- packages/typography/src/index.js
+++ packages/typography/src/index.js
@@ -9,6 +9,9 @@ import {
   maxWidth,
 } from 'styled-system'
 import pick from 'lodash.pick'
+import omit from 'lodash.omit'
+import flatten from 'lodash.flattendeep'
+import merge from 'lodash.merge'
 
 import * as themes from './themes'
 export { reset } from './reset'
@@ -47,13 +50,39 @@ export const tagNames = [
   'abbr',
 ]
 
+export const systemProps = [
+  'theme',
+  'm', 'mt', 'mr', 'mb', 'ml', 'mx', 'my',
+  'p', 'pt', 'pr', 'pb', 'pl', 'px', 'py',
+  'margin',
+  'marginTop',
+  'marginRight',
+  'marginBottom',
+  'marginLeft',
+  'padding',
+  'paddingTop',
+  'paddingRight',
+  'paddingBottom',
+  'paddingLeft',
+  'fontFamily',
+  'fontSize',
+  'fontWeight',
+  'lineHeight',
+  'color',
+  'bg',
+  'backgroundColor',
+]
+
+const css = props => omit(props, systemProps)
+
 export const typographyStyles = compose(
   fontFamily,
   fontSize,
   fontWeight,
   lineHeight,
   space,
-  color
+  color,
+  css
 )
 
 export const typography = props => {
@@ -66,9 +95,9 @@ export const typography = props => {
     const el = elements[key]
     const rules = typographyStyles({ theme, ...el })
     if (key === 'body') {
-      styles['&'] = [ el, ...rules ]
+      styles['&'] = merge(...flatten(rules))
     }
-    styles[key] = [ el, ...rules ]
+    styles[key] = merge(...flatten(rules))
   }
 
   return styles
