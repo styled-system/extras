8b415e14ce59e41168afe9faab0241483d47d15e
Brent Jackson <jxnblk@gmail.com>
2019-04-27 17:05:46 -0400

    Fix css utility in babel plugin

--- packages/babel-plugin/css.js
+++ packages/babel-plugin/css.js
@@ -1,10 +1,74 @@
 import get from 'lodash.get'
 
 const keys = {
-  color: 'colors',
-  backgroundColor: 'colors',
-  borderColor: 'colors',
+  fontSize: 'fontSizes',
+  fontFamily: 'fonts',
+  fontWeight: 'fontWeights',
+  letterSpacing: 'letterSpacings',
 }
+const setKeys = (key, props) => {
+  props.forEach(prop => {
+    keys[prop] = key
+  })
+}
+
+setKeys('colors', [
+  'color',
+  'backgroundColor',
+  'borderColor',
+])
+
+setKeys('space', [
+  'margin',
+  'marginTop',
+  'marginRight',
+  'marginBottom',
+  'marginLeft',
+  'marginX',
+  'marginY',
+  'padding',
+  'paddingTop',
+  'paddingRight',
+  'paddingBottom',
+  'paddingLeft',
+  'paddingX',
+  'paddingY',
+  'm',
+  'mt',
+  'mr',
+  'mb',
+  'ml',
+  'mx',
+  'my',
+  'p',
+  'pt',
+  'pr',
+  'pb',
+  'pl',
+  'px',
+  'py',
+])
+
+setKeys('sizes', [
+  'width',
+  'height',
+  'minWidth',
+  'maxWidth',
+  'minHeight',
+  'maxHeight',
+])
+
+setKeys('borders', [
+  'border',
+  'borderTop',
+  'borderRight',
+  'borderBottom',
+  'borderLeft',
+])
+setKeys('borderWidths', ['borderWidth'])
+setKeys('borderStyles', ['borderStyle'])
+setKeys('radii', ['borderRadius'])
+setKeys('zIndices', ['zIndex'])
 
 export const css = styles => theme => {
   const result = {}
@@ -15,7 +79,6 @@ export const css = styles => theme => {
       continue
     }
     result[key] = get(theme, `${keys[key]}.${value}`, value)
-    const themeKey
   }
   return result
 }
