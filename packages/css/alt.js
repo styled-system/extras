d37b7d95e34c9fe8f113ca198dd97763ddf181bd
Brent Jackson <jxnblk@gmail.com>
2019-04-30 13:42:26 -0400

    Add alternative implementation of css package

---
+++ packages/css/alt.js
@@ -0,0 +1,130 @@
+import get from 'lodash.get'
+
+const defaultBreakpoints = [40, 52, 64].map(n => n + 'em')
+
+// for compatibility with current version
+const defaultTheme = {
+  space: [
+    0, 4, 8, 16, 32, 64, 128, 256, 512
+  ],
+  fontSizes: [
+    12, 14, 16, 20, 24, 32, 48, 64, 72
+  ],
+}
+
+const aliases = {
+  bg: 'backgroundColor',
+  m: 'margin',
+  mt: 'marginTop',
+  mr: 'marginRight',
+  mb: 'marginBottom',
+  ml: 'marginLeft',
+  mx: [ 'marginLeft', 'marginRight' ],
+  my: [ 'marginTop', 'marginBottom' ],
+  p: 'padding',
+  pt: 'paddingTop',
+  pr: 'paddingRight',
+  pb: 'paddingBottom',
+  pl: 'paddingLeft',
+  px: [ 'paddingLeft', 'paddingRight' ],
+  py: [ 'paddingTop', 'paddingBottom' ],
+}
+
+const scales = {
+  color: 'colors',
+  backgroundColor: 'colors',
+  borderColor: 'colors',
+  margin: 'space',
+  marginTop: 'space',
+  marginRight: 'space',
+  marginBottom: 'space',
+  marginLeft: 'space',
+  padding: 'space',
+  paddingTop: 'space',
+  paddingRight: 'space',
+  paddingBottom: 'space',
+  paddingLeft: 'space',
+  fontFamily: 'fonts',
+  fontSize: 'fontSizes',
+  fontWeight: 'fontWeights',
+  lineHeight: 'lineHeights',
+  letterSpacing: 'letterSpacings',
+  border: 'borders',
+  borderTop: 'borders',
+  borderRight: 'borders',
+  borderBottom: 'borders',
+  borderLeft: 'borders',
+  borderWidth: 'borderWidths',
+  borderStyle: 'borderStyles',
+  borderRadius: 'radii',
+  boxShadow: 'shadows',
+  zIndex: 'zIndices',
+  // new/breaking
+  width: 'sizes',
+  minWidth: 'sizes',
+  maxWidth: 'sizes',
+  height: 'sizes',
+  minHeight: 'sizes',
+  maxHeight: 'sizes',
+}
+
+export const responsive = styles => theme => {
+  const next = {}
+  const breakpoints = get(theme, 'breakpoints', [ '40em', '52em', '64em' ])
+  const mediaQueries = [ null, ...breakpoints.map(n => `@media screen and (min-width: ${n})`) ]
+
+  for (const key in styles) {
+    const value = styles[key]
+    if (value && !Array.isArray(value) && typeof value === 'object') {
+      next[key] = responsive(value)(theme)
+      continue
+    }
+    if (!Array.isArray(value)) {
+      next[key] = value
+      continue
+    }
+    value.forEach((val, i) => {
+      const media = mediaQueries[i]
+      if (!media) {
+        next[key] = val
+        return
+      }
+      next[media] = next[media] || {}
+      next[media][key] = val
+    })
+  }
+
+  return next
+}
+
+export const css = args => (props = {}) => {
+  const theme = { ...defaultTheme, ...(props.theme || props) }
+  const result = {}
+  const obj = typeof args === 'function' ? args(theme) : args
+  const styles = responsive(obj)(theme)
+
+  for (const key in styles) {
+    const prop = aliases[key] || key
+    const scaleName = scales[prop] || scales[prop[0]]
+    const scale = get(theme, scaleName, get(theme, prop, {}))
+    const x = styles[key]
+    // hot new shit
+    const val = typeof x === 'function' ? x(theme) : x
+    if (val && typeof val === 'object') {
+      result[prop] = css(val)(theme)
+      continue
+    }
+    const value = get(scale, val, val)
+    if (Array.isArray(prop)) {
+      prop.forEach(p => {
+        result[p] = value
+      })
+    } else {
+      result[prop] = value
+    }
+  }
+
+  return result
+}
+
+export default css
--- packages/css/test.js
+++ packages/css/test.js
@@ -1,6 +1,6 @@
 import test from 'ava'
 import { width, space } from 'styled-system'
-import css, { createCSS } from './index'
+import css, { createCSS } from './alt'
 
 const theme = {
   colors: {
@@ -38,7 +38,7 @@ test('returns styles', t => {
     borderRadius: 4,
   })()
   t.deepEqual(res, {
-    fontSize: '32px',
+    fontSize: 32,
     color: 'blue',
     borderRadius: 4,
   })
@@ -50,12 +50,12 @@ test('returns system props styles', t => {
     fontSize: [ 2, 3, 4]
   })({ theme })
   t.deepEqual(res, {
-    fontSize: '16px',
+    fontSize: 16,
     '@media screen and (min-width: 40em)': {
-      fontSize: '24px',
+      fontSize: 24,
     },
     '@media screen and (min-width: 52em)': {
-      fontSize: '36px',
+      fontSize: 36,
     },
     color: 'tomato',
   })
@@ -86,17 +86,18 @@ test('returns nested responsive styles', t => {
   t.deepEqual(res, {
     color: 'tomato',
     h1: {
-      paddingTop: '16px',
-      paddingBottom: '16px',
+      paddingTop: 16,
+      paddingBottom: 16,
       '@media screen and (min-width: 40em)': {
-        paddingTop: '32px',
-        paddingBottom: '32px',
+        paddingTop: 32,
+        paddingBottom: 32,
       }
     }
   })
 })
 
-test('props override default styles', t => {
+// breaking change
+test.skip('props override default styles', t => {
   const res = css({
     color: 'primary',
   })({ theme, color: 'secondary' })
@@ -121,16 +122,16 @@ test('handles all core styled system props', t => {
   })({ theme })
   t.deepEqual(res, {
     margin: 0,
-    marginBottom: '8px',
+    marginBottom: 8,
     marginLeft: 'auto',
     marginRight: 'auto',
-    padding: '16px',
-    paddingTop: '32px',
-    paddingBottom: '32px',
+    padding: 16,
+    paddingTop: 32,
+    paddingBottom: 32,
     color: 'tomato',
     backgroundColor: 'cyan',
     fontFamily: 'Menlo, monospace',
-    fontSize: '24px',
+    fontSize: 24,
     fontWeight: 600,
     lineHeight: 1.5,
   })
@@ -145,11 +146,13 @@ test('works with the css prop', t => {
   t.deepEqual(res, {
     color: 'tomato',
     margin: 0,
-    fontSize: '16px',
+    fontSize: 16,
   })
 })
 
-test('createCSS returns a custom css function', t => {
+test.todo('handles custom transforms')
+
+test.skip('createCSS returns a custom css function', t => {
   const func = createCSS([ width, space ])
   const res = func({
     width: [ 1/2, 1/4 ],
@@ -162,7 +165,7 @@ test('createCSS returns a custom css function', t => {
     '@media screen and (min-width: 40em)': {
       width: '25%',
     },
-    margin: '16px',
+    margin: 16,
     color: 'primary',
   })
 })
