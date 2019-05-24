d1072866d339f29fd86f7b80b3bfad1be3de9501
Brent Jackson <jxnblk@gmail.com>
2019-05-24 17:04:38 -0400

    Export get utility

--- packages/css/index.js
+++ packages/css/index.js
@@ -1,5 +1,5 @@
 // based on https://github.com/developit/dlv
-const get = (obj, key = '', def, p, undef) => {
+export const get = (obj, key = '', def, p, undef) => {
   key = key.split ? key.split('.') : [key]
   for (p = 0; p < key.length; p++) {
     obj = obj ? obj[key[p]] : undef
